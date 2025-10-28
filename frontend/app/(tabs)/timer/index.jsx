5df37aef45aadc9af4ed8bf85b3c2226e28e341d
Alanis-C3 <acastillo3@ufl.edu>
2025-10-27 20:33:48 -0400

    frontend skeleton for timer page (timer box and headers completed)

--- frontend/app/(tabs)/timer/index.jsx
+++ frontend/app/(tabs)/timer/index.jsx
@@ -2,20 +2,61 @@ import React from "react";
 import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
 import Colors from "@/constants/Colors";
 
-export default function Home() {
+export default function Timer() {
   return (
-    <View style={styles.mockpage}>
-      <Text>Timer</Text>
+    <View style={styles.page}>
+      <View style = {styles.headerRow}>
+      <Text style ={styles.header}>Let's get studying!</Text>
+      </View>
+      <View style={styles.contentRow}>
+        <View style={styles.timerContainer}>
+          </View>
+        </View>
+        <View style={styles.taskRow}>
+          <Text style={styles.subHeader}> Today's Session: </Text>
+          </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  mockpage: {
+  page: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
     backgroundColor: Colors.backgroundBlue,
     color: Colors.backgroundBlue,
+    
+  },
+   headerRow: {
+    paddingTop: 25,
+    paddingHorizontal: 5,
+  },
+  contentRow: {
+    paddingTop: 25,                
+    alignItems: "center", 
+  },
+  taskRow:{
+    alignItems: "center",
+    paddingVertical: 16,
+    zIndex: 10,
+  },
+  header: {
+    color: "white",
+    fontSize: 60,
+    textAlign: "center",
+    fontWeight: "bold",
+  },
+  subHeader:{
+    color: "white",
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+  timerContainer:{
+    justifyContent: "center",
+    backgroundColor: Colors.lightBlue,
+    borderColor: Colors.borderGray,
+    borderWidth: 5,
+    borderRadius: 10,
+    height: 460,
+    width: "80%",
   },
 });
